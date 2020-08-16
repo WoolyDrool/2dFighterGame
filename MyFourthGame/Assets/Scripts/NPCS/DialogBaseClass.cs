e2a209e271ee2e9eded884cd8ff6d93f5bd88402
WoolyDrool <nikolai.mcneely15@gmail.com>
2020-08-16 01:00:55 -0700

    Added dialog system
    Added a dialog system based off of
    this unity tutorial: https://www.youtube.com/watch?v=8eJ_gxkYsyY

---
+++ MyFourthGame/Assets/Scripts/NPCS/DialogBaseClass.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace DialogSystem 
+{
+public class DialogBaseClass : MonoBehaviour
+{
+    protected IEnumerator WriteText(string input, Text textHolder, float delay, AudioClip sound)
+    {
+        textHolder.text = "";
+        //textHolder.color = textColor;
+        for(int i=0; i<input.Length; i++)
+        {
+            textHolder.text += input[i];
+            SoundManager.instance.PlaySound(sound);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+   
+}
+
+}
+
---
+++ MyFourthGame/Assets/Scripts/NPCS/DialogLine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace DialogSystem{
+public class DialogLine : DialogBaseClass
+{
+    [Header("Text Options")]
+    [SerializeField]private string input;
+    [SerializeField]Text textHolder;
+
+    [Header("Time parameters")]
+    [SerializeField]private float delay;
+
+    [Header("Sound")]
+    [SerializeField] private AudioClip sound;
+
+    private void Awake() 
+    {
+        textHolder = GetComponent<Text>();
+
+        StartCoroutine(WriteText(input, textHolder, delay, sound));
+    }
+}
+}
